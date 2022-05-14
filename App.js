8e1a10a312fe3ef0ea99ad2653dc7cb5e0bd3a05
zerontec <zerontecnologia1@gmail.com>
2022-05-14 01:00:12 -0400

    Screem home Detail

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import HomeScreem from './components/HomeScreem';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import Detail from './components/Detail';
+
+const Stack = createStackNavigator();
+
+
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Sorry</Text>
-      <StatusBar style="auto" />
-    </View>
+<NavigationContainer>
+<Stack.Navigator>
+   
+   <Stack.Screen 
+   
+   name= 'Home'
+   component={HomeScreem}
+   />
+ <Stack.Screen 
+   
+   name= 'Detail'
+   component={Detail}
+   />
+
+ 
+
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
