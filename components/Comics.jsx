ce79b987723b3b5894bf70b8bebd4b4933199cd6
zerontec <zerontecnologia1@gmail.com>
2022-05-14 21:34:01 -0400

    Comic Component

--- components/Comics.jsx
+++ components/Comics.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import {View, Text} from 'react-native'
 
 export default function Comics() {
   return (
-    <div>Comics</div>
+<View>
+
+<Text>Estas en Comic</Text>
+
+</View>
   )
 }
