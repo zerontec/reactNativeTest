ba686574e4f0f010a24ebbdb8cb189f9e6396bed
zerontec <zerontecnologia1@gmail.com>
2022-05-14 21:29:42 -0400

    component Character

--- components/CharacterCard.jsx
+++ components/CharacterCard.jsx
@@ -3,29 +3,19 @@ import {Text, View, Image} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const CharacterCard = ({image, name}) => {
-
+const CharacterCard = ({ image, name }) => {
+    
+    
     const navigation = useNavigation();
 
+    return (
 
-return (
+        <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
+        <Image source={image} />
+        <Text>{name}</Text>
+        </TouchableOpacity>
 
-<TouchableOpacity
-style={styles.container }
-onPress={() => navigation.naviagate('Detail')}
-
->
-<Image styles={styles.image}
-source={image} />
-
-<Text styles={styles.font}>{name} </Text>
-
-
-
-
-</TouchableOpacity>
-
-)
+    )
 
 
 }
